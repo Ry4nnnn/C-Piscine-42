4de93d2ae265d0a97a08b054522fd31e097b36c5
Wei Jun Lim <wlim@u82z10s04.42kl.edu.my>
2025-05-01 01:13:11 +0800

    added: RUSH00

--- RUSH00/main.c
+++ RUSH00/main.c
@@ -1,53 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wlim <wlim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/21 12:21:14 by ryan              #+#    #+#             */
-/*   Updated: 2025/04/29 22:00:48 by wlim             ###   ########.fr       */
+/*   Created: 2025/04/11 21:39:58 by wlim              #+#    #+#             */
+/*   Updated: 2025/04/13 13:38:56 by wlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
-int	*ft_range(int min, int max)
+int		rush(int x, int y);
+int		ft_atoi(char *str);
+void	ft_putstr(char *str);
+
+int	main(int ac, char **av)
 {
-	int	i;
-	int	*arr;
-
-	i = 0;
-	if (min >= max)
-		return (NULL);
-	arr = (int *)malloc(sizeof(int) * (max - min));
-	if (!arr)
-		return (NULL);
-	while (min < max)
-	{
-		arr[i] = min;
-		min++;
-		i++;
-	}
-	return (arr);
-}
-
-/*
-#include <stdio.h>
-int	main()
-{
-	int i = 0;
-	int a = 5;
-	int b = 10;
-	int *arr = ft_range(a, b);
-	
-	while (i < (b - a))
-	{
-		printf ("%d\n", arr[i]);
-		i++;
-	}
-	free (arr);
+	if (ac == 3)
+		rush(ft_atoi(av[1]), ft_atoi(av[2]));
+	else
+		ft_putstr("Argument count must be 3\n");
 	return (0);
 }
-*/
---
+++ RUSH00/rush01.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlim <wlim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 23:04:52 by wlim              #+#    #+#             */
+/*   Updated: 2025/04/13 10:55:43 by wlim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_atoi(char *str);
+void	ft_putstr(char *str);
+void	ft_putchar(char a);
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+		write(1, str++, 1);
+}
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+
+static void	print_row(int x, char left, char mid, char right)
+{
+	int	col;
+
+	col = 0;
+	if (x >= 1)
+		ft_putchar(left);
+	while (col < x - 2)
+	{
+		ft_putchar(mid);
+		col++;
+	}
+	if (x > 1)
+		ft_putchar(right);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	row;
+
+	row = 0;
+	if (x <= 0 || y <= 0)
+	{
+		ft_putstr("number must be more than 0\n");
+		return ;
+	}
+	while (row < y)
+	{
+		if (row == 0)
+			print_row(x, '/', '*', '\\');
+		else if (row == y - 1)
+			print_row(x, '\\', '*', '/');
+		else
+			print_row(x, '*', ' ', '*');
+		row++;
+	}
+}
