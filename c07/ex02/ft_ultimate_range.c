18d75e86c0638bd3a15789dd48edc7d0323c679c
Ryan Lim <ryan@Ryys-Macbook-Air.local>
2025-05-01 00:39:10 +0800

    fixed critical bug: c07/ex02

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wlim <wlim@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ryan <ryan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:31:38 by ryan              #+#    #+#             */
-/*   Updated: 2025/04/30 21:57:46 by wlim             ###   ########.fr       */
+/*   Updated: 2025/05/01 00:36:33 by ryan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@ int	ft_ultimate_range(int **range, int min, int max)
 	int	i;
 
 	i = 0;
-	*range = (int *)malloc(sizeof(int) * (max - min));
 	if (max <= min)
 	{
 		*range = NULL;
 		return (0);
 	}
+	*range = (int *)malloc(sizeof(int) * (max - min));
 	if (!*range)
-	{
-		*range = NULL;
 		return (-1);
-	}
 	while (min < max)
 	{
-		*range[i] = min;
+		(*range)[i] = min;
 		min++;
 		i++;
 	}
